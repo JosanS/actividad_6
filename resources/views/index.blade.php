61b5f4e209e9d49b5891824e571f4a334d4e9cd9
JosanS <josansalinas@hotmail.com>
2024-09-28 12:03:30 -0600

    homework 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,11 @@
     <title>Product View</title>
 </head>
 <body>
+    <h1>Product Index</h1>
+
+    <a href="{{ route('products.create') }}">
+        <button>Add Product</button>
+    </a>
     
 </body>
 </html>
